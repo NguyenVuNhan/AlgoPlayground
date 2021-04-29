1788e6ba1552d685539bf0232328c5db3b048342
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-04-29 21:34:55 +0200

    Align menu icon and side nav icons

--- libs/web/ui/components/src/lib/header/header.tsx
+++ libs/web/ui/components/src/lib/header/header.tsx
@@ -9,9 +9,9 @@ export function Header(props: HeaderProps) {
   const { onMenuClick } = props;
 
   return (
-    <header className="w-full bg-gray-900 px-3 py-6 text-white flex aligns-center shadow-e3">
+    <header className="w-full bg-gray-900 pl-0 px-3 py-6 text-white flex aligns-center shadow-e3">
       <button
-        className="btn icon-btn"
+        className="btn icon-btn mx-4"
         onClick={onMenuClick}
         data-testid="menu-btn"
       >
