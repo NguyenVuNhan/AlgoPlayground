d10ab2d67bceab56238a204c146e331c64cdb292
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-04-30 23:15:39 +0200

    Add wall checking

--- libs/simulation/physics/src/lib/bouncing-ball/bouncing-ball.tsx
+++ libs/simulation/physics/src/lib/bouncing-ball/bouncing-ball.tsx
@@ -13,6 +13,7 @@ class Ball {
   x: number;
   y: number;
   vy: number;
+  vx: number;
   fill: string;
   active: boolean;
 
@@ -21,16 +22,53 @@ class Ball {
     this.x = x;
     this.y = y;
     this.vy = 1;
+    this.vx = 1;
     this.fill = randomColor();
     this.active = true;
   }
 
-  move() {
-    if (!this.active) return;
+  collision(balls: Ball[]) {
+    balls.forEach((ball) => {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const d = (dx ** 2 + dy ** 2) ** 0.5;
+        const rr = this.radius + ball.radius;
 
-    this.vy += gravity;
-    this.y += this.vy;
+        if (d < rr) {
+          const angle = Math.atan2(dy, dx);
+          const spread = rr - d;
+          const ax = spread * Math.cos(angle);
+          const ay = spread * Math.sin(angle);
 
+          this.x -= ax;
+          this.y -= ay;
+
+          this.vx *= -bounce;
+          this.vy *= -bounce;
+          ball.vx *= -bounce;
+          ball.vy *= -bounce;
+        }
+      }
+    });
+  }
+
+  checkWall() {
+    // Left boundary
+    const leftBorder = 0 + this.radius;
+    if (this.x < leftBorder) {
+      this.x += bounce * (leftBorder - this.x);
+      this.vx *= -bounce;
+    }
+
+    // Right boundary
+    const rightBorder = width - this.radius;
+    if (this.x > rightBorder) {
+      this.x -= bounce * (this.x - rightBorder);
+      this.vx *= -bounce;
+    }
+
+    // Bottom boundary
     if (this.y + this.radius > height) {
       this.y -= bounce * (this.y - (height - this.radius));
       this.vy *= -bounce;
@@ -42,6 +80,16 @@ class Ball {
         this.active = false;
       }
     }
+  }
+
+  move() {
+    if (!this.active) return;
+
+    this.vy += gravity;
+    this.y += this.vy;
+    this.x += this.vx;
+
+    this.checkWall();
   }
 }
 
@@ -96,6 +144,7 @@ export function BouncingBall(props: BouncingBallProps) {
     let i = 0;
     while (i < balls.length) {
       balls[i++].move();
+      // balls[i++].collision(balls);
     }
 
     setBalls([...balls]);
